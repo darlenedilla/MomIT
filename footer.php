bc2ce07bcafadd1f06dac1af885277b0030d9c93
darlenedilla <darlenedilla@hotmail.com>
2019-12-06 13:11:18 +0100

    Validation changes

--- footer.php
+++ footer.php
@@ -1,31 +1,31 @@
 
 <footer>
 
-<div id="footer-sidebar" class="secondary">
+    <div id="footer-sidebar" class="secondary">
 
-    <div id="footer-sidebar1">
-        <p class="footer-sidebar-text">Skabt i samarbejde med:</p>
-        <?php
-        if(is_active_sidebar('footer-sidebar-1')){
-        dynamic_sidebar('footer-sidebar-1');
-        }
-        ?>
+        <div id="footer-sidebar1">
+            <p class="footer-sidebar-text">Skabt i samarbejde med:</p>
+            <?php
+            if(is_active_sidebar('footer-sidebar-1')){
+            dynamic_sidebar('footer-sidebar-1');
+            }
+            ?>
+        </div>
+
+        <span class="footerLongLine"></span>
+
+        <div class="footer-sidebar2">
+            <div class="footer-download">
+                    <a href="https://apps.apple.com/dk/app/mit-sygehus/id836616671?l=da" target="_blank">
+                        <img class="download-buttons" src="<?php echo get_stylesheet_directory_uri(); ?>/img/appstore.png" alt="Appstore link">
+                    </a>
+                    <a href="https://play.google.com/store/apps/details?id=dk.medware.mitforloeb&hl=da" target="_blank">
+                        <img class="download-buttons" src="<?=get_stylesheet_directory_uri(); ?>/img/googleplay.png" alt="Google play link">
+                    </a>
+                </div>
+        </div>
+
     </div>
-
-    <span class="footerLongLine"></span>
-
-    <div class="footer-sidebar2">
-         <div class="footer-download">
-                <a href="https://apps.apple.com/dk/app/mit-sygehus/id836616671?l=da" target="_blank">
-                    <img class="download-buttons" src="<?php echo get_stylesheet_directory_uri(); ?>/img/appstore.png" alt="Appstore link">
-                </a>
-                <a href="https://play.google.com/store/apps/details?id=dk.medware.mitforloeb&hl=da" target="_blank">
-                    <img class="download-buttons" src="<?=get_stylesheet_directory_uri(); ?>/img/googleplay.png" alt="Google play link">
-                </a>
-            </div>
-    </div>
-
-</div>
-</body>
 </footer>
+</body>
 </html>
